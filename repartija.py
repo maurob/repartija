8e35658398674b8f6b5e67b6e0627efea1bc0037
Mauro Bruni <maumarbru@gmail.com>
2015-05-30 17:16:02 -0300

    Separo el modelo Repartija en repartija.py y los tests en tests.py

---
+++ repartija.py
@@ -0,0 +1,58 @@
+# -- coding=utf-8 --
+
+class Repartija:
+    """ Permite definir los precios individuales por hora de cada persona que trabaja 
+    y calcular las ganancias individuales, exponiendo un único precio por hora para
+    quien se trabaja. """
+
+    def set_precios_por_hora(self, precios_por_hora):
+        """ El *precio_por_hora* es un diccionario tipo `{'mauro':170, 'martin':230}` """
+        self.precios_por_hora = {individuo: float(precio) for individuo, precio in precios_por_hora.items()}
+
+    def set_trabajo(self, horas):
+        """ Define la cantidad de horas trabajadas por cada individuo en el diccionario *horas* """
+        self.horas_trabajadas = {individuo: float(precio) for individuo, precio in horas.items()}
+        
+    def individuos(self):
+        """ Devuelve la lista de individuos """
+        return self.precios_por_hora.keys()
+
+    def precio_medio_por_hora(self):
+        """ Es el precio que se va a cobrar por hora trabajada """
+        return sum(self.precios_por_hora.values()) / len(self.precios_por_hora)
+
+    def ganancia_individual(self):
+        """ Es el valor que se lleva aca individuo """
+        try:
+            ajuste = self.precio_real() / self.precio_deceado()
+        except ZeroDivisionError: # Porque los único que trabajaron trabajan grátis
+            d = {None: self.precio_real()}
+            d.update({individuo: 0.0 for individuo in self.individuos()})
+            return d
+        
+        return {individuo: self.horas_trabajadas[individuo] * precio_por_hora * ajuste
+                for individuo, precio_por_hora in self.precios_por_hora.items()}
+        
+    def precios_por_hora_ajustado(self):
+        """ Es el precio por hora individual correjido, que depende de las horas trabajadas """
+        ajuste = self.ajuste()
+        return {individuo: precio_por_hora * ajuste
+                for individuo, precio_por_hora in self.precios_por_hora.items()}
+
+    def ajuste(self):
+        """ Coeficiente de ajuste para alcanzar el precio real a partir del precio deceado """
+        return self.precio_real() / self.precio_deceado()
+
+    def total_de_horas_trabajadas(self):
+        """ Es la suma de las horas trabajas de cada uno """
+        return sum(self.horas_trabajadas.values())
+        
+    def precio_real(self):
+        """ Es el precio que se va a cobrar """
+        return float(self.precio_medio_por_hora() * self.total_de_horas_trabajadas())
+    
+    def precio_deceado(self):
+        """ Es el precio que se pretende cobrar en función del precio horario de cada uno """
+        return float(sum(self.horas_trabajadas[individuo] * precio_por_hora
+                   for individuo, precio_por_hora in self.precios_por_hora.items()))
+ 
---
+++ tests.py
@@ -0,0 +1,87 @@
+# -- coding=utf-8 --
+
+import unittest
+from repartija import Repartija
+
+class Test(unittest.TestCase):
+
+    def test_precio_de_hora_igual(self):
+        """ Si cobramos igual la hora, deberíamos repartirnos en función de las horas trabajadas """
+        uut = Repartija()
+        uut.set_precios_por_hora({'martin': 10, 'mauro': 10})
+        
+        # Igual horas trabajadas
+        uut.set_trabajo({'martin': 1, 'mauro': 1})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], r['mauro'])
+
+        # Sólo uno trabaja
+        uut.set_trabajo({'martin': 0, 'mauro': 1})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 0)
+        self.assertEqual(r['mauro'], 10)
+        
+    def test_precio_de_hora_diferentes(self):
+        """ Si cobramos diferente la hora pero trabajamos la misma cantidad de horas,
+        las ganancias debería ser directamente el precio por hora individual por la
+        cantidad de horas. """
+        uut = Repartija()
+        uut.set_precios_por_hora({'martin': 230, 'mauro': 170})
+
+        uut.set_trabajo({'martin': 1, 'mauro': 1})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 230)
+        self.assertEqual(r['mauro'], 170)
+
+        uut.set_trabajo({'martin': 2, 'mauro': 2})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 230*2)
+        self.assertEqual(r['mauro'], 170*2)
+        
+    def test_uno_no_trabaja(self):
+        """ Si uno no trabaja el otro tiene que cobrar el precio_medio_por_hora,
+        es decir el precio expuesto para el cliente """
+        uut = Repartija()
+        uut.set_precios_por_hora({'martin': 230, 'mauro': 170})
+        self.assertEqual(uut.precio_medio_por_hora(), 200)
+
+        uut.set_trabajo({'martin': 1, 'mauro': 0})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 200)
+        self.assertEqual(r['mauro'], 0)
+        
+    def test_uno_trabaja_gratis(self):
+        """ Si uno trabaja gratis el otro se ve afectado por cuanto trabaje el gratuito """
+        uut = Repartija()
+        uut.set_precios_por_hora({'martin': 100, 'mauro': 0})
+        self.assertEqual(uut.precio_medio_por_hora(), 50)
+        
+        # El gratuito no trabaja
+        uut.set_trabajo({'martin': 1, 'mauro': 0})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 50)
+        self.assertEqual(r['mauro'], 0)
+
+        # El gratuito trabaja lo mismo
+        uut.set_trabajo({'martin': 1, 'mauro': 1})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 100)
+        self.assertEqual(r['mauro'], 0)
+
+        # El gratuito trabaja más
+        uut.set_trabajo({'martin': 1, 'mauro': 2})
+        r = uut.ganancia_individual()
+        self.assertEqual(r['martin'], 150)
+        self.assertEqual(r['mauro'], 0)
+
+        # Sólo el gratuito trabaja y sobra plata
+        uut.set_trabajo({'martin': 0, 'mauro': 1})
+        r = uut.ganancia_individual()
+        self.assertEqual(r[None], 50)
+        self.assertEqual(r['mauro'], 0)
+        self.assertEqual(r['martin'], 0)
+
+if __name__=='__main__':
+    unittest.main()
+#    suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test)
+#    unittest.TextTestRunner().run(suite)
